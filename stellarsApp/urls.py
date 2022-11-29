268c184a3e2ec98dff45ccd515a0790d59024294
Patricio Pagano <pagano.patricio@gmail.com>
2022-11-29 19:34:34 -0300

    cambios en html

--- stellarsApp/urls.py
+++ stellarsApp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 # -------------------- Vistas a importar -------------------- #
 from stellarsApp import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="Inicio"),
@@ -12,3 +13,6 @@ urlpatterns = [
     path('blog/', views.blog, name="Blog"),
     path('base', views.base, name="Base"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
